a0e705632bc74dc863d3e32760ead5dcc19c64f4
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-24 21:10:55 +0530

    day 4

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import { signup} from "../controller/auth.controllers.js";
 
+
 // Routes
 router.post("/signup", signup);// router.post("/login", login);
 // router.get("/logout", logout);
