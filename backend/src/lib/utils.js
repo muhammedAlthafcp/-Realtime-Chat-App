1680914cac316c06af2cc482aef729ef4a36923a
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-25 22:24:08 +0530

    day 5

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,6 +5,7 @@ export const generateToken = (userId) => {
   if(!JWT_SECRET){
     throw new console.error("not now");
     
+    
   }
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "7d", // token expiry (adjust as needed)
