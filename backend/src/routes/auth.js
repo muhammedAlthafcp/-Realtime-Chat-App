8e3e650a7d8daef2bb6e19ebc28e5558e7b3a6ca
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-28 00:20:47 +0530

    day 8

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -8,7 +8,8 @@ import { signup,login,logout,updateProfile} from "../controller/auth.controllers
 router.post("/signup", signup);
 router.post("/login",login)
 router.post("/logout", logout);
-router.post("/update-profile", protectRouter ,updateProfile);     
+router.post("/update-profile", protectRouter ,updateProfile);   
+router.get("/check", protectRouter, (req, res) => res.status(200).json(req.user));
 
 export default router;
 
