e1964868bd28c023ee73d97b00dae220918763a7
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-28 23:58:36 +0530

    day 8

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 cloudinary.config({
-  cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
-  api_key: ENV.CLOUDINARY_API_KEY,
-  api_secret: ENV.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
