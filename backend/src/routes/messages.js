6c92634cda4d54183d3a9c3f500eb192ffc83db7
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-29 21:57:16 +0530

    day 9

--- backend/src/routes/messages.js
+++ backend/src/routes/messages.js
@@ -4,8 +4,6 @@ import express from "express";
 const router = express.Router();
 
 // Example route
-router.get("/newchat", (req, res) => {
-  res.json({ message: "Messages route is working ✅" });
-});
+
 
 export default router;
