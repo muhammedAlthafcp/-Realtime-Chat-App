829737faf576b07362f8ee3883ff80d5aceff344
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-30 11:52:04 +0530

    day 10

--- backend/src/routes/messages.js
+++ backend/src/routes/messages.js
@@ -2,15 +2,16 @@
 import express from "express";
 
 const router = express.Router();
-import { getAllContacts,getMessagesByUserId } from "../controller/message.router.js";
+import { getAllContacts,getMessagesByUserId,sendMessage, getChatPartners } from "../controller/message.router.js";
 import { protectRouter } from "../middlewere/auth.middlewere.js"
 
 
 
 // Example route
 router.get("/contacts",protectRouter, getAllContacts);
-// router.get("/chats", getChatPartners);
-router.get("/:id", getMessagesByUserId);
+router.get("/chats",protectRouter, getChatPartners);
+router.get("/:id",protectRouter, getMessagesByUserId);
+router.post("/send/:id",protectRouter, sendMessage);
 
 
 
