20959bd05823461133394aeb6414c89f4a2f2d70
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-10-07 23:21:51 +0530

    day 14

--- frontend/src/pages/signUpPage.jsx
+++ frontend/src/pages/signUpPage.jsx
@@ -15,6 +15,7 @@ export default function SignUpPage() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     signup(formData);
